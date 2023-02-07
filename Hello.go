31b6941cf860e6876d982db8dc1ae4dea2fd2ced
Denis Uglov <denisuglov1981@gmail.com>
2023-02-07 21:40:09 +0600

    feat: add web app feat

--- Hello.go
+++ Hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!!!")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }

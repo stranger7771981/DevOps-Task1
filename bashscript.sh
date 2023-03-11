f356f8a6e6c3cc1cb3cbbc60e5eaf6ecaae50515
Uglov Denis <36145126+stranger7771981@users.noreply.github.com>
2023-03-11 00:29:26 -0500

    bashsript - change ip host

--- bashscript.sh
+++ bashscript.sh
@@ -2,4 +2,4 @@
 
 echo "connecting to server, docker pull and run docker container"
 
-ssh -T devops@192.168.31.164  "docker pull free1as2bird/devopstask1:latest && docker run --hostname=7c540686e618 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -p 8080:8080 --label='Name=devopstask1' --runtime=runc -d free1as2bird/devopstask1:latest"
+ssh -T devops@192.46.236.15  "docker pull free1as2bird/devopstask1:latest && docker run --hostname=7c540686e618 --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin -p 8080:8080 --label='Name=devopstask1' --runtime=runc -d free1as2bird/devopstask1:latest"
